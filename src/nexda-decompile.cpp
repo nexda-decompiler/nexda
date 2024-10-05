5c51ca40de2ddd88a03cd43477ad143cd32748ce
VoltagedDebunked <rusindanilo@gmail.com>
2024-10-05 17:45:55 +0300

    Added cross platform support

--- src/nexda-decompile.cpp
+++ src/nexda-decompile.cpp
@@ -1,44 +1,51 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <vector>
-#include <map>
+#include <capstone/capstone.h>
 #include <cstring>
 #include <fcntl.h>
-#include <unistd.h>
-#include <libelf.h>
+#include <fstream>
 #include <gelf.h>
-#include <capstone/capstone.h>
+#include <iomanip>
+#include <iostream>
+#include <libelf.h>
+#include <map>
+#include <unistd.h>
+#include <vector>
 
-class Decompiler {
-private:
-    Elf* elf;
+class Decompiler
+{
+  private:
+    Elf *elf;
     int fd;
     csh handle;
     std::ofstream outFile;
     std::map<uint64_t, std::string> functions;
 
-public:
-    Decompiler(const char* inputFile, const char* outputFile) : elf(nullptr), fd(-1) {
-        if (elf_version(EV_CURRENT) == EV_NONE) {
+  public:
+    Decompiler(const char *inputFile, const char *outputFile)
+        : elf(nullptr), fd(-1)
+    {
+        if (elf_version(EV_CURRENT) == EV_NONE)
+        {
             std::cerr << "Failed to initialize libelf" << std::endl;
             return;
         }
 
         fd = open(inputFile, O_RDONLY);
-        if (fd < 0) {
+        if (fd < 0)
+        {
             std::cerr << "Failed to open file: " << inputFile << std::endl;
             return;
         }
 
         elf = elf_begin(fd, ELF_C_READ, nullptr);
-        if (!elf) {
+        if (!elf)
+        {
             std::cerr << "Failed to read ELF file" << std::endl;
             close(fd);
             return;
         }
 
-        if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+        if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK)
+        {
             std::cerr << "Failed to initialize Capstone" << std::endl;
             elf_end(elf);
             close(fd);
@@ -46,8 +53,10 @@ public:
         }
 
         outFile.open(outputFile);
-        if (!outFile.is_open()) {
-            std::cerr << "Failed to open output file: " << outputFile << std::endl;
+        if (!outFile.is_open())
+        {
+            std::cerr << "Failed to open output file: " << outputFile
+                      << std::endl;
             elf_end(elf);
             close(fd);
             cs_close(&handle);
@@ -55,57 +64,77 @@ public:
         }
     }
 
-    ~Decompiler() {
-        if (elf) elf_end(elf);
-        if (fd >= 0) close(fd);
+    ~Decompiler()
+    {
+        if (elf)
+            elf_end(elf);
+        if (fd >= 0)
+            close(fd);
         cs_close(&handle);
-        if (outFile.is_open()) outFile.close();
+        if (outFile.is_open())
+            outFile.close();
     }
 
-    void decompile() {
+    void
+    decompile()
+    {
         analyzeFunctions();
         generateCCode();
     }
 
-private:
-    void analyzeFunctions() {
-        Elf_Scn* scn = nullptr;
+  private:
+    void
+    analyzeFunctions()
+    {
+        Elf_Scn *scn = nullptr;
         GElf_Shdr shdr;
         size_t shstrndx;
 
-        if (elf_getshdrstrndx(elf, &shstrndx) != 0) {
-            std::cerr << "Failed to get section header string index" << std::endl;
+        if (elf_getshdrstrndx(elf, &shstrndx) != 0)
+        {
+            std::cerr << "Failed to get section header string index"
+                      << std::endl;
             return;
         }
 
-        while ((scn = elf_nextscn(elf, scn)) != nullptr) {
-            if (gelf_getshdr(scn, &shdr) != &shdr) {
+        while ((scn = elf_nextscn(elf, scn)) != nullptr)
+        {
+            if (gelf_getshdr(scn, &shdr) != &shdr)
+            {
                 std::cerr << "Failed to get section header" << std::endl;
                 continue;
             }
 
-            const char* name = elf_strptr(elf, shstrndx, shdr.sh_name);
-            if (name == nullptr) {
+            const char *name = elf_strptr(elf, shstrndx, shdr.sh_name);
+            if (name == nullptr)
+            {
                 std::cerr << "Failed to get section name" << std::endl;
                 continue;
             }
 
-            if (shdr.sh_type == SHT_SYMTAB) {
+            if (shdr.sh_type == SHT_SYMTAB)
+            {
                 analyzeSymbolTable(scn, &shdr);
             }
         }
     }
 
-    void analyzeSymbolTable(Elf_Scn* scn, GElf_Shdr* shdr) {
-        Elf_Data* data = elf_getdata(scn, nullptr);
+    void
+    analyzeSymbolTable(Elf_Scn *scn, GElf_Shdr *shdr)
+    {
+        Elf_Data *data = elf_getdata(scn, nullptr);
         int count = shdr->sh_size / shdr->sh_entsize;
 
-        for (int i = 0; i < count; ++i) {
+        for (int i = 0; i < count; ++i)
+        {
             GElf_Sym sym;
-            if (gelf_getsym(data, i, &sym) == &sym) {
-                if (GELF_ST_TYPE(sym.st_info) == STT_FUNC) {
-                    char* name = elf_strptr(elf, shdr->sh_link, sym.st_name);
-                    if (name) {
+            if (gelf_getsym(data, i, &sym) == &sym)
+            {
+                if (GELF_ST_TYPE(sym.st_info) == STT_FUNC)
+                {
+                    char *name = elf_strptr(elf, shdr->sh_link, sym.st_name);
+                    if (name)
+                    {
                         functions[sym.st_value] = name;
                     }
                 }
@@ -113,10 +142,13 @@ private:
         }
     }
 
-    void generateCCode() {
+    void
+    generateCCode()
+    {
         outFile << "#include <stdio.h>\n\n";
 
-        for (const auto& func : functions) {
+        for (const auto &func : functions)
+        {
             outFile << "void " << func.second << "() {\n";
             disassembleFunction(func.first);
             outFile << "}\n\n";
@@ -128,17 +160,24 @@ private:
         outFile << "}\n";
     }
 
-    void disassembleFunction(uint64_t address) {
-        Elf_Scn* scn = nullptr;
+    void
+    disassembleFunction(uint64_t address)
+    {
+        Elf_Scn *scn = nullptr;
         GElf_Shdr shdr;
 
-        while ((scn = elf_nextscn(elf, scn)) != nullptr) {
-            if (gelf_getshdr(scn, &shdr) != &shdr) {
+        while ((scn = elf_nextscn(elf, scn)) != nullptr)
+        {
+            if (gelf_getshdr(scn, &shdr) != &shdr)
+            {
                 continue;
             }
 
-            if (shdr.sh_type == SHT_PROGBITS && (shdr.sh_flags & SHF_EXECINSTR)) {
-                if (address >= shdr.sh_addr && address < shdr.sh_addr + shdr.sh_size) {
+            if (shdr.sh_type == SHT_PROGBITS && (shdr.sh_flags & SHF_EXECINSTR))
+            {
+                if (address >= shdr.sh_addr &&
+                    address < shdr.sh_addr + shdr.sh_size)
+                {
                     disassembleSection(scn, &shdr, address);
                     break;
                 }
@@ -146,57 +185,79 @@ private:
         }
     }
 
-    void disassembleSection(Elf_Scn* scn, GElf_Shdr* shdr, uint64_t startAddress) {
-        Elf_Data* data = elf_getdata(scn, nullptr);
-        if (data == nullptr || data->d_size == 0) {
+    void
+    disassembleSection(Elf_Scn *scn, GElf_Shdr *shdr, uint64_t startAddress)
+    {
+        Elf_Data *data = elf_getdata(scn, nullptr);
+        if (data == nullptr || data->d_size == 0)
+        {
             std::cerr << "Failed to get section data" << std::endl;
             return;
         }
 
-        uint8_t* code = static_cast<uint8_t*>(data->d_buf);
+        uint8_t *code = static_cast<uint8_t *>(data->d_buf);
         size_t code_size = data->d_size;
         uint64_t address = shdr->sh_addr;
         size_t offset = startAddress - address;
 
-        cs_insn* insn;
-        size_t count = cs_disasm(handle, code + offset, code_size - offset, startAddress, 0, &insn);
+        cs_insn *insn;
+        size_t count = cs_disasm(handle, code + offset, code_size - offset,
+                                 startAddress, 0, &insn);
 
-        if (count > 0) {
-            for (size_t j = 0; j < count; j++) {
+        if (count > 0)
+        {
+            for (size_t j = 0; j < count; j++)
+            {
                 generatePseudoCode(insn[j]);
 
                 // Stop at return instruction
-                if (std::string(insn[j].mnemonic) == "ret") {
+                if (std::string(insn[j].mnemonic) == "ret")
+                {
                     break;
                 }
             }
 
             cs_free(insn, count);
-        } else {
+        }
+        else
+        {
             std::cerr << "Failed to disassemble function" << std::endl;
         }
     }
 
-    void generatePseudoCode(const cs_insn& insn) {
+    void
+    generatePseudoCode(const cs_insn &insn)
+    {
         std::string mnemonic(insn.mnemonic);
         std::string op_str(insn.op_str);
 
         outFile << "    ";
 
-        if (mnemonic == "mov") {
-            outFile << op_str.substr(op_str.find(',') + 1) << " = " << op_str.substr(0, op_str.find(',')) << ";\n";
-        } else if (mnemonic == "call") {
+        if (mnemonic == "mov")
+        {
+            outFile << op_str.substr(op_str.find(',') + 1) << " = "
+                    << op_str.substr(0, op_str.find(',')) << ";\n";
+        }
+        else if (mnemonic == "call")
+        {
             outFile << op_str << "();\n";
-        } else if (mnemonic == "ret") {
+        }
+        else if (mnemonic == "ret")
+        {
             outFile << "return;\n";
-        } else {
+        }
+        else
+        {
             outFile << "// " << mnemonic << " " << op_str << "\n";
         }
     }
 };
 
-int main(int argc, char** argv) {
-    if (argc != 4 || strcmp(argv[2], "-o") != 0) {
+int
+main(int argc, char **argv)
+{
+    if (argc != 4 || strcmp(argv[2], "-o") != 0)
+    {
         std::cerr << "Usage: " << argv[0] << " input.elf -o out.c" << std::endl;
         return 1;
     }
--- src/nexda-disasm.cpp
+++ src/nexda-disasm.cpp
@@ -1,68 +1,169 @@
-#include <iostream>
-#include <fstream>
+#include <capstone/capstone.h>
 #include <cstring>
 #include <fcntl.h>
-#include <unistd.h>
-#include <libelf.h>
+#include <fstream>
 #include <gelf.h>
-#include <capstone/capstone.h>
+#include <iostream>
+#include <libelf.h>
+#include <unistd.h>
 
-void disassemble(const std::string& inputFile, const std::string& outputFile, bool pure) {
-    int fd;
-    Elf* elf;
-    Elf_Scn* scn = NULL;
-    GElf_Shdr shdr;
-    Elf_Data* data;
-    char* section_name;
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#endif
 
-    if (elf_version(EV_CURRENT) == EV_NONE) {
-        std::cerr << "Error: ELF library initialization failed: " << elf_errmsg(-1) << std::endl;
+enum FileType
+{
+    UNKNOWN,
+    ELF_FILE,
+    PE_FILE
+};
+
+FileType
+detect_file_type(int fd)
+{
+    unsigned char magic[4];
+    if (read(fd, magic, 4) != 4)
+    {
+        return UNKNOWN;
+    }
+
+    if (memcmp(magic,
+               "\x7F"
+               "ELF",
+               4) == 0)
+    {
+        return ELF_FILE;
+    }
+    else if (memcmp(magic, "MZ", 2) == 0)
+    {
+        return PE_FILE;
+    }
+
+    return UNKNOWN;
+}
+
+// PE header structures
+struct DOSHeader
+{
+    uint16_t e_magic; // MZ signature
+    uint16_t e_cblp;
+    uint16_t e_cp;
+    uint16_t e_crlc;
+    uint16_t e_cparhdr;
+    uint16_t e_minalloc;
+    uint16_t e_maxalloc;
+    uint16_t e_ss;
+    uint16_t e_sp;
+    uint16_t e_csum;
+    uint16_t e_ip;
+    uint16_t e_cs;
+    uint16_t e_lfarlc;
+    uint16_t e_ovno;
+    uint16_t e_res[4];
+    uint16_t e_oemid;
+    uint16_t e_oeminfo;
+    uint16_t e_res2[10];
+    uint32_t e_lfanew; // Offset to PE header
+};
+
+struct PEHeader
+{
+    uint32_t signature; // PE\0\0
+    uint16_t machine;
+    uint16_t numberOfSections;
+    uint32_t timeDateStamp;
+    uint32_t pointerToSymbolTable;
+    uint32_t numberOfSymbols;
+    uint16_t sizeOfOptionalHeader;
+    uint16_t characteristics;
+};
+
+struct SectionHeader
+{
+    char name[8];
+    uint32_t virtualSize;
+    uint32_t virtualAddress;
+    uint32_t sizeOfRawData;
+    uint32_t pointerToRawData;
+    uint32_t pointerToRelocations;
+    uint32_t pointerToLinenumbers;
+    uint16_t numberOfRelocations;
+    uint16_t numberOfLinenumbers;
+    uint32_t characteristics;
+};
+
+void
+disassemble_pe(int fd, const std::string &outputFile, bool pure)
+{
+    lseek(fd, 0, SEEK_SET);
+
+    DOSHeader dosHeader;
+    if (read(fd, &dosHeader, sizeof(DOSHeader)) != sizeof(DOSHeader))
+    {
+        std::cerr << "Error: Cannot read DOS header" << std::endl;
         return;
     }
 
-    if ((fd = open(inputFile.c_str(), O_RDONLY, 0)) < 0) {
-        std::cerr << "Error: Cannot open file " << inputFile << std::endl;
+    if (dosHeader.e_magic != 0x5A4D)
+    { // 'MZ'
+        std::cerr << "Error: Invalid PE file (MZ signature missing)"
+                  << std::endl;
         return;
     }
 
-    if ((elf = elf_begin(fd, ELF_C_READ, NULL)) == NULL) {
-        std::cerr << "Error: elf_begin() failed: " << elf_errmsg(-1) << std::endl;
-        close(fd);
+    lseek(fd, dosHeader.e_lfanew, SEEK_SET); // Jump to PE header
+
+    PEHeader peHeader;
+    if (read(fd, &peHeader, sizeof(PEHeader)) != sizeof(PEHeader))
+    {
+        std::cerr << "Error: Cannot read PE header" << std::endl;
         return;
     }
 
-    size_t shstrndx;
-    if (elf_getshstrndx(elf, &shstrndx) != 0) {
-        std::cerr << "Error: elf_getshstrndx() failed: " << elf_errmsg(-1) << std::endl;
-        elf_end(elf);
-        close(fd);
+    if (peHeader.signature != 0x00004550)
+    { // 'PE\0\0'
+        std::cerr << "Error: Invalid PE signature" << std::endl;
         return;
     }
 
-    while ((scn = elf_nextscn(elf, scn)) != NULL) {
-        gelf_getshdr(scn, &shdr);
-        if ((section_name = elf_strptr(elf, shstrndx, shdr.sh_name)) == NULL) {
-            std::cerr << "Error: elf_strptr() failed: " << elf_errmsg(-1) << std::endl;
-            elf_end(elf);
-            close(fd);
+    SectionHeader sectionHeader;
+    bool textSectionFound = false;
+    uint32_t textSectionOffset = 0, textSectionSize = 0, textSectionVA = 0;
+
+    for (int i = 0; i < peHeader.numberOfSections; i++)
+    {
+        if (read(fd, &sectionHeader, sizeof(SectionHeader)) !=
+            sizeof(SectionHeader))
+        {
+            std::cerr << "Error: Cannot read section header" << std::endl;
             return;
         }
-        if (strcmp(section_name, ".text") == 0) {
+
+        if (strncmp(sectionHeader.name, ".text", 8) == 0)
+        {
+            textSectionOffset = sectionHeader.pointerToRawData;
+            textSectionSize = sectionHeader.sizeOfRawData;
+            textSectionVA = sectionHeader.virtualAddress;
+            textSectionFound = true;
             break;
         }
     }
 
-    if (scn == NULL) {
-        std::cerr << "Error: .text section not found" << std::endl;
-        elf_end(elf);
-        close(fd);
+    if (!textSectionFound)
+    {
+        std::cerr << "Error: .text section not found in PE file" << std::endl;
         return;
     }
 
-    if ((data = elf_getdata(scn, NULL)) == NULL) {
-        std::cerr << "Error: elf_getdata() failed: " << elf_errmsg(-1) << std::endl;
-        elf_end(elf);
-        close(fd);
+    // Read the .text section
+    uint8_t *textSectionData = new uint8_t[textSectionSize];
+    lseek(fd, textSectionOffset, SEEK_SET);
+    if (read(fd, textSectionData, textSectionSize) != textSectionSize)
+    {
+        std::cerr << "Error: Cannot read .text section" << std::endl;
+        delete[] textSectionData;
         return;
     }
 
@@ -70,10 +171,111 @@ void disassemble(const std::string& inputFile, const std::string& outputFile, bo
     cs_insn *insn;
     size_t count;
 
+    if (cs_open(CS_ARCH_X86, CS_MODE_32, &handle) != CS_ERR_OK)
+    {
+        std::cerr << "Error: Capstone initialization failed" << std::endl;
+        delete[] textSectionData;
+        return;
+    }
+
+    std::ofstream outFile(outputFile);
+    if (!outFile.is_open())
+    {
+        std::cerr << "Error: Cannot open output file" << outputFile
+                  << std::endl;
+        cs_close(&handle);
+        delete[] textSectionData;
+        return;
+    }
+
+    count = cs_disasm(handle, textSectionData, textSectionSize, textSectionVA,
+                      0, &insn);
+    if (count > 0)
+    {
+        for (size_t j = 0; j < count; j++)
+        {
+            if (pure)
+            {
+                outFile << insn[j].mnemonic << " " << insn[j].op_str
+                        << std::endl;
+            }
+            else
+            {
+                outFile << std::hex << insn[j].address << ": "
+                        << insn[j].mnemonic << " " << insn[j].op_str
+                        << std::endl;
+            }
+        }
+        cs_free(insn, count);
+    }
+    else
+    {
+        std::cerr << "Error: Disassembly failed" << std::endl;
+    }
+
+    outFile.close();
+    cs_close(&handle);
+    delete[] textSectionData;
+}
+
+void
+disassemble_elf(int fd, const std::string& outputFile, bool pure) {
+    Elf* elf;
+    Elf_Scn* scn = NULL;
+    GElf_Shdr shdr;
+    Elf_Data* data;
+
+    if (elf_version(EV_CURRENT) == EV_NONE) {
+        std::cerr << "Error: ELF library initialization failed: " << elf_errmsg(-1) << std::endl;
+        return;
+    }
+
+    if ((elf = elf_begin(fd, ELF_C_READ, NULL)) == NULL) {
+        std::cerr << "Error: elf_begin() failed: " << elf_errmsg(-1) << std::endl;
+        return;
+    }
+
+    size_t shstrndx;
+    if (elf_getshstrndx(elf, &shstrndx) != 0) {
+        std::cerr << "Error: elf_getshstrndx() failed: " << elf_errmsg(-1) << std::endl;
+        elf_end(elf);
+        return;
+    }
+
+    bool textSectionFound = false;
+    while ((scn = elf_nextscn(elf, scn)) != NULL) {
+        gelf_getshdr(scn, &shdr);
+        const char* section_name = elf_strptr(elf, shstrndx, shdr.sh_name);
+        if (section_name == NULL) {
+            std::cerr << "Error: elf_strptr() failed: " << elf_errmsg(-1) << std::endl;
+            elf_end(elf);
+            return;
+        }
+        if (strcmp(section_name, ".text") == 0) {
+            textSectionFound = true;
+            break;
+        }
+    }
+
+    if (!textSectionFound) {
+        std::cerr << "Error: .text section not found" << std::endl;
+        elf_end(elf);
+        return;
+    }
+
+    if ((data = elf_getdata(scn, NULL)) == NULL) {
+        std::cerr << "Error: elf_getdata() failed: " << elf_errmsg(-1) << std::endl;
+        elf_end(elf);
+        return;
+    }
+
+    csh handle;
+    cs_insn* insn;
+    size_t count;
+
     if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
         std::cerr << "Error: Capstone initialization failed" << std::endl;
         elf_end(elf);
-        close(fd);
         return;
     }
 
@@ -82,7 +284,6 @@ void disassemble(const std::string& inputFile, const std::string& outputFile, bo
         std::cerr << "Error: Cannot open output file " << outputFile << std::endl;
         cs_close(&handle);
         elf_end(elf);
-        close(fd);
         return;
     }
 
@@ -90,12 +291,9 @@ void disassemble(const std::string& inputFile, const std::string& outputFile, bo
     if (count > 0) {
         for (size_t j = 0; j < count; j++) {
             if (pure) {
-                outFile << insn[j].mnemonic << " "
-                        << insn[j].op_str << std::endl;
+                outFile << insn[j].mnemonic << " " << insn[j].op_str << std::endl;
             } else {
-                outFile << std::hex << insn[j].address << ": "
-                        << insn[j].mnemonic << " "
-                        << insn[j].op_str << std::endl;
+                outFile << std::hex << insn[j].address << ": " << insn[j].mnemonic << " " << insn[j].op_str << std::endl;
             }
         }
         cs_free(insn, count);
@@ -106,12 +304,43 @@ void disassemble(const std::string& inputFile, const std::string& outputFile, bo
     outFile.close();
     cs_close(&handle);
     elf_end(elf);
+}
+
+void
+disassemble(const std::string &inputFile, const std::string &outputFile,
+            bool pure)
+{
+    int fd;
+    if ((fd = open(inputFile.c_str(), O_RDONLY, 0)) < 0)
+    {
+        std::cerr << "Error: Cannot open file " << inputFile << std::endl;
+        return;
+    }
+
+    FileType fileType = detect_file_type(fd);
+    switch (fileType)
+    {
+    case PE_FILE:
+        disassemble_pe(fd, outputFile, pure);
+        break;
+    case ELF_FILE:
+        disassemble_elf(fd, outputFile, pure);
+        break;
+    default:
+        std::cerr << "Error: Unknown file type" << std::endl;
+        break;
+    }
+
     close(fd);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 4 || std::string(argv[2]) != "-o") {
-        std::cerr << "Usage: " << argv[0] << " <input_file> -o <output_file> [-pure]" << std::endl;
+int
+main(int argc, char *argv[])
+{
+    if (argc < 4 || std::string(argv[2]) != "-o")
+    {
+        std::cerr << "Usage: " << argv[0]
+                  << " <input_file> -o <output_file> [-pure]" << std::endl;
         return 1;
     }
 
